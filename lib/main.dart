21120fcda9eda4860c9f12d6976dc035763cb950
snt-nbzw <sntnbzw@gmail.com>
2021-03-11 11:19:20 +0900

    Add: implement drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
+  runApp(MaterialApp(
+    home: DrawerSamplePage(),
+  ));
+}
+
+class DrawerSamplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drawer Demo'),
+      ),
+      body: Center(child: Text('My Page!')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
